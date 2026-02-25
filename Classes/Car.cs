119c8cec8b04137e1b1cd405eb605a82f0eaa1b8
Chirag Patel <chirag.patel.learning@gmail.com>
2026-02-25 08:35:11 -0600

    Car class put in a separate file

---
+++ Classes/Car.cs
@@ -0,0 +1,22 @@
+﻿namespace Classes;
+
+    // Creating a new public class blueprint
+    public class Car
+    {
+        /* default Constructor
+        public Car()
+        {
+        }
+        */
+    
+        // Property 'Make'
+        public string Make { get; set; }
+        
+        // Property 'Model'
+        public string Model { get; set; }
+        
+        // Property 'Year'
+        public int Year { get; set; }
+        
+        
+    }
--- Classes/obj/Debug/net9.0/Classes.AssemblyInfo.cs
+++ Classes/obj/Debug/net9.0/Classes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Classes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e78ea86b89d4d0a593035f4affd23de3dbab37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d8e523e4111b6fa7c8c72c015a3be32c1216337")]
 [assembly: System.Reflection.AssemblyProductAttribute("Classes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Classes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
